6a22568d025a96bf9538ebaa4de6a6705066e8b7
nicolas <nicolas.ferracioli.silva@escola.pr.gov.br>
2024-11-18 14:37:34 -0300

    aula2

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Animando o scroll para as seções
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      window.scrollTo({
+          top: targetElement.offsetTop - 20,
+          behavior: 'smooth'
+      });
+  });
+});
